9940782035c11b3e63129c58565b4dd7511f1fbf
Jaggu <jagadeesh6010@gmail.com>
2020-06-03 17:14:34 -0400

    Day 5

--- Applications/Notes.js
+++ Applications/Notes.js
@@ -1,5 +1,44 @@
 const notes = ['Note1', 'Note2', 'Note3']
 
-console.log(notes.length);
-console.log(notes[notes.length-1]);
+// console.log(notes.length);
+// console.log(notes[notes.length-1]);
 
+// push - add item to array
+// pop  - removes an item from the end and returns the deleted value
+
+console.log(notes.pop());
+console.log(notes);
+notes.push('New Note')
+console.log(notes);
+
+// shift - remove item from beginning
+// unshift - add item to beginning
+console.log(notes.shift())
+// console.log(notes);
+
+notes.unshift('New Note 2')
+// console.log(notes);
+
+// splice - add / remove item from given index
+
+notes.splice(1,1)
+// console.log(notes);
+
+notes.splice(1,0,'Splice note 1')
+// console.log(notes);
+
+notes.splice(1,1,'Replace splicce Note 2')
+// console.log(notes);
+
+// For each
+    notes.forEach(function(element , index) {
+        console.log(`${index+1} . ${element}`);
+        
+    });
+
+    // For Loop
+    for(let count = 0 ; count<notes.length;count++){
+        console.log(`${count+1} . ${notes[count]}`);    
+    }
+
+    
--- Applications/Todo.js
+++ Applications/Todo.js
@@ -3,7 +3,35 @@
 // Print first and second to last items -> Todo: todo1
 const todo = ['Python', 'JavaScript', 'Eggplant', 'CypressIo', 'Postman']
 
-console.log(`You have ${todo.length} todo's`);
-console.log(`Todo: ${todo[todo.length-1]}`)
-console.log(`Todo: ${todo[todo.length-2]}`)
+// console.log(`You have ${todo.length} todo's`);
+// console.log(`Todo: ${todo[todo.length-1]}`)
+// console.log(`Todo: ${todo[todo.length-2]}`)
+
+
+// Delete 3rd item
+
+todo.splice(2,1)
+// console.log(todo);
+
+// Add New item onto the end
+todo.push('ML')
+// console.log(todo);
+
+// Remove first item from list
+todo.shift()
+// console.log(todo);
+
+// 1. first item
+// 2. second item
+
+// Using For each loop
+todo.forEach(function(element , index) {
+    console.log(`${index+1} . ${element}`);
+    
+});
+
+// Using for loop
+for(let count = 0 ; count<todo.length;count++){
+    console.log(`${count+1} . ${todo[count]}`);    
+}
 
