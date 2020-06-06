fb78e5f4a14f35070f826d81991e9c2eb92d6d53
Jaggu <jagadeesh6010@gmail.com>
2020-06-06 14:42:04 -0400

    Day 6

--- Applications/Notes.js
+++ Applications/Notes.js
@@ -6,14 +6,14 @@ const notes = ['Note1', 'Note2', 'Note3']
 // push - add item to array
 // pop  - removes an item from the end and returns the deleted value
 
-console.log(notes.pop());
-console.log(notes);
+// console.log(notes.pop());
+// console.log(notes);
 notes.push('New Note')
-console.log(notes);
+// console.log(notes);
 
 // shift - remove item from beginning
 // unshift - add item to beginning
-console.log(notes.shift())
+// console.log(notes.shift())
 // console.log(notes);
 
 notes.unshift('New Note 2')
@@ -27,18 +27,57 @@ notes.splice(1,1)
 notes.splice(1,0,'Splice note 1')
 // console.log(notes);
 
-notes.splice(1,1,'Replace splicce Note 2')
+notes.splice(1,1,'Replace splice Note 2')
 // console.log(notes);
 
 // For each
     notes.forEach(function(element , index) {
-        console.log(`${index+1} . ${element}`);
+        // console.log(`${index+1} . ${element}`);
         
     });
 
     // For Loop
     for(let count = 0 ; count<notes.length;count++){
-        console.log(`${count+1} . ${notes[count]}`);    
+        // console.log(`${count+1} . ${notes[count]}`);    
     }
 
-    
+// console.log(notes.indexOf('New Note 2'));
+
+
+notes_objects = [
+    {
+        title: 'Title 1',
+        note : 'Note 1'
+    },
+    {
+        title: 'Title 2',
+        note : 'Note 2'
+    },
+    {
+        title: 'Title 3',
+        note : 'Note 3'
+    }
+]
+
+const index = notes_objects.findIndex(function(note,index){
+    return note.title === 'Title 2'
+})
+
+// console.log(index);
+
+const findNote = function(notes, noteTitle){
+  
+    const index = notes.findIndex(function(note,index){
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+    return notes[index]
+}
+
+const findNote1 = function(notes, noteTitle){
+    return notes.find(function(note,index){
+        return note.title.toLowerCase() === noteTitle.toLowerCase()
+    })
+}
+
+console.log(findNote(notes_objects, 'Title 3'));
+console.log(findNote1(notes_objects, 'Title 1'));
--- Applications/Todo.js
+++ Applications/Todo.js
@@ -1,7 +1,7 @@
 // Create array with five todos
 // You have 5 todos
 // Print first and second to last items -> Todo: todo1
-const todo = ['Python', 'JavaScript', 'Eggplant', 'CypressIo', 'Postman']
+// const todo = ['Python', 'JavaScript', 'Eggplant', 'CypressIo', 'Postman']
 
 // console.log(`You have ${todo.length} todo's`);
 // console.log(`Todo: ${todo[todo.length-1]}`)
@@ -10,28 +10,85 @@ const todo = ['Python', 'JavaScript', 'Eggplant', 'CypressIo', 'Postman']
 
 // Delete 3rd item
 
-todo.splice(2,1)
+// todo.splice(2,1)
 // console.log(todo);
 
 // Add New item onto the end
-todo.push('ML')
+// todo.push('ML')
 // console.log(todo);
 
 // Remove first item from list
-todo.shift()
+// todo.shift()
 // console.log(todo);
 
 // 1. first item
 // 2. second item
 
 // Using For each loop
-todo.forEach(function(element , index) {
-    console.log(`${index+1} . ${element}`);
+// todo.forEach(function(element , index) {
+//     console.log(`${index+1} . ${element}`);
     
-});
+// });
 
 // Using for loop
-for(let count = 0 ; count<todo.length;count++){
-    console.log(`${count+1} . ${todo[count]}`);    
+// for(let count = 0 ; count<todo.length;count++){
+//     console.log(`${count+1} . ${todo[count]}`);    
+// }
+
+
+// 1. Convert array into array of objects - text, completed
+// 2. Create function to remove a todo by text value
+
+const todo = [
+    {
+        item: 'Python',
+        completed : '50%'
+    },
+
+    {
+        item: 'JavaScript',
+        completed : '5%'
+    },
+
+    {
+        item: 'EggPlant',
+        completed : '1%'
+    },
+
+    {
+        item: 'CypressIO',
+        completed : '5%'
+    },
+
+    {
+        item: 'PostMan',
+        completed : '15%'
+    },
+    
+    {
+        item: 'Java',
+        completed : '75%'
+
+    }
+]
+
+const getIndex = function(todos, text){
+    const index = todos.findIndex(function(todo,index){
+        return todo.item.toLowerCase() === text.toLowerCase()
+    })
+    return index
 }
 
+
+function deleteToDo(todos, text){
+    const index = getIndex(todos, text)
+    console.log(index);
+    
+    if(index > -1)
+        todos.splice(index,1)
+
+}
+
+deleteToDo(todo, 'Java')
+
+console.log(todo);
