5e0918851e6af3c98f2de61b4af88c65ed8ebb48
Jaggu <jagadeesh6010@gmail.com>
2020-06-10 21:24:31 -0400

    Day 10

--- JS/assets/scripts/app.js
+++ JS/assets/scripts/app.js
@@ -1,10 +1,40 @@
-let currentResult = 0;
+const defaultRes = 0
+let currentResult = defaultRes;
+
 
 // currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-function add(num1, num2){
-    result = currentResult + parseInt(userInput.value)
-    outputResult(result,'');
+// function descriptionn(operator, initialResult, enteredNumber){
+//     const desc = `${initialResult} ${operator} ${enteredNumber}`
+
+// }
+
+
+function add(){
+    const desc = `${currentResult} + ${userInput.value}`
+    currentResult += parseInt(userInput.value)
+    outputResult(currentResult,desc);
+}
+
+function sub(){
+    const desc = `${currentResult} - ${userInput.value}`
+    currentResult -= parseInt(userInput.value)
+    outputResult(currentResult,desc);
+}
+
+function mul(){
+    const desc = `${currentResult} * ${userInput.value}`
+    currentResult *= parseInt(userInput.value)
+    outputResult(currentResult,desc);
+}
+
+function div(){
+    const desc = `${currentResult} / ${userInput.value}`
+    currentResult /= parseInt(userInput.value)
+    outputResult(currentResult,desc);
 }
 
 addBtn.addEventListener('click', add)
+subtractBtn.addEventListener('click', sub)
+multiplyBtn.addEventListener('click', mul)
+divideBtn.addEventListener('click', div)
